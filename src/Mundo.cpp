0d62f4a30b19891a8f1bda707ca3c073fda3a4dc
ramon <ramonmorcillocascales@gmail.com>
2019-04-12 03:08:06 -0700

    vistas work. falta depurar y HUD

--- src/Mundo.cpp
+++ src/Mundo.cpp
@@ -23,6 +23,24 @@ Mundo::Mundo()
     p1 = new Jugador();
     e1 = new Enemigo();
 
+    /** HUD, vista and minimap stuff **/
+    // HUD
+    player_lifes = 5;
+    num_pizzas = 0;
+    txt_pizza = new Texture();
+    txt_pizza -> loadFromFile("assets/hud/pizza.png");
+    spr_pizza = new Sprite(*txt_pizza);
+    spr_pizza ->setPosition(100,100);
+//    spr_pizza -> setScale(50.f/190, 50.f/200);
+    // Vista
+    vista = new View();
+    vista -> reset(sf::FloatRect(p1->getPhysicsState().getActualState()[0],p1->getPhysicsState().getActualState()[1], 300, 200));
+    vista->setCenter((float)p1->getPhysicsState().getActualState()[0], (float)p1->getPhysicsState().getActualState()[1]);
+    vista->setViewport(sf::FloatRect(0.f, 0.f, 1.f, 1.f));
+    // Minimap
+    minimap = new View();
+    minimap->setViewport(sf::FloatRect(0.75f, 0.75f, 0.25f, 0.25f));
+
 }
 
 int Mundo::getPtoEntregaActual()
@@ -79,7 +97,7 @@ void Mundo::updateMundo(bool eRight, bool eLeft, bool eUp, bool eDown, sf::Time 
     {
         cout << "COLISIONA" << endl;
 
-        eLeft = false;
+//        eLeft = false;
     }
     else
     {
@@ -89,6 +107,12 @@ void Mundo::updateMundo(bool eRight, bool eLeft, bool eUp, bool eDown, sf::Time 
     p1->updateJugador(eRight,eLeft,eUp,eDown,t);
     e1->updateEnemigo(p1->getPhysicsState().getActualState()[0],p1->getPhysicsState().getActualState()[1],t);
     procesarColisiones();
+
+    /** Updateamos la updatecamara para que updatesiga al updatejugador **/
+//    vista->setCenter(p1->getPhysicsState().getActualState()[0],p1->getPhysicsState().getActualState()[1]);
+//    vista->setCenter(p1->renderPos[0],p1->renderPos[1]);
+        vista->move(p1->getPhysicsState().getActualState()[0] - p1->getPhysicsState().getPastState()[0], p1->getPhysicsState().getActualState()[1] - p1->getPhysicsState().getPastState()[1]);
+
 
 
 }
@@ -113,7 +137,45 @@ void Mundo::drawMundo(sf::RenderWindow * ventana, double inter)
     }
     p1->drawJugador(ventana,inter);
     e1->drawEnemigo(ventana,inter);
+    ventana->setView(*minimap);
+    // Hasta aqui el minimap.
 
+    mapa->draw(ventana);
+    ventana->draw(*ptoEntrega);
+    // Dibujo los powerUps
+    for( int i = 0; i < items.size(); i++ )
+    {
+        float duracion = items[i]->getClock().getElapsedTime().asSeconds();
+        if(duracion > items[i]->getTiempoGeneracion() && duracion < items[i]->getTiempoVida() + items[i]->getTiempoGeneracion())
+        {
+            ventana->draw(items[i]->getSprite());
+        }
+        if(duracion > items[i]->getTiempoVida() + items[i]->getTiempoGeneracion())
+        {
+            items[i]->restartPowerUp();
+        }
+
+    }
+    p1->drawJugador(ventana,inter);
+    e1->drawEnemigo(ventana,inter);
+    ventana->setView(*vista);
+        // Hasta aqui la vista principal.
+
+    //
+//    //Dibujamos nuestras mierdas
+//
+//    mapa->draw(ventana);
+//    p1->drawJugador(ventana,i);
+//
+//    ventana->setView(*minimap);
+//
+//    mapa->draw(ventana);
+//    p1->drawJugador(ventana,i);
+//    ventana->setView(*vista);
+//
+//     ventana->draw(*spr_pizza);
+//
+//    ventana->display();
 
 }
 
