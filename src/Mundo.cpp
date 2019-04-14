966d92cc50570552866bd0e977a763eff863fedd
ramon <ramonmorcillocascales@gmail.com>
2019-04-14 09:29:37 -0700

    working on lifes

--- src/Mundo.cpp
+++ src/Mundo.cpp
@@ -37,12 +37,18 @@ Mundo::Mundo()
 
     font_numbers = new Font();
     font_numbers ->loadFromFile("assets/hud/fullPack2025.ttf");
+    font_player_lifes = new Font();
+    font_player_lifes ->loadFromFile("assets/hud/HeartsSt.ttf");
     text_num_pizzas = new Text();
     text_num_pizzas -> setFont(*font_numbers);
     text_num_pizzas -> setCharacterSize(15);
     text_time = new Text();
     text_time -> setFont(*font_numbers);
     text_time -> setCharacterSize(15);
+    text_player_lifes = new Text();
+    text_player_lifes -> setFont(*font_player_lifes);
+    text_player_lifes -> setCharacterSize(15);
+    text_player_lifes -> setColor(Color::Red);
 
     // Vista
     vista = new View();
@@ -163,7 +169,15 @@ void Mundo::processHUD()
  *time1 = clock1->getElapsedTime();
  text_num_pizzas -> setString(std::to_string(num_pizzas));
  text_time -> setString(std::to_string(time1->asSeconds()));
+
+ string strAux;
+    for (int i = 0; i<player_lifes; i++ )
+    {
+        strAux.append("f");
+    }
+    text_player_lifes -> setString(strAux);
 }
+
 
 void Mundo::drawItems(sf::RenderWindow * ventana)
 {
@@ -204,11 +218,13 @@ void Mundo::drawMundo(sf::RenderWindow * ventana, double inter)
     e1->drawEnemigo(ventana,inter);
     /// HUD STUFF
     spr_pizza ->setPosition(p1->getSprite()->getRenderPos()[0],p1->getSprite()->getRenderPos()[1] - 100);
-    text_num_pizzas -> setPosition(p1->getSprite()->getRenderPos()[0]+20,p1->getSprite()->getRenderPos()[1] - 100);
-    text_time -> setPosition(p1->getSprite()->getRenderPos()[0]+70,p1->getSprite()->getRenderPos()[1] - 100);
     ventana->draw(*spr_pizza);
+    text_num_pizzas -> setPosition(p1->getSprite()->getRenderPos()[0]+20,p1->getSprite()->getRenderPos()[1] - 100);
     ventana->draw(*text_num_pizzas);
+    text_time -> setPosition(p1->getSprite()->getRenderPos()[0]+70,p1->getSprite()->getRenderPos()[1] - 100);
     ventana->draw(*text_time);
+    text_player_lifes -> setPosition(p1->getSprite()->getRenderPos()[0]-70,p1->getSprite()->getRenderPos()[1] - 100);
+    ventana->draw(*text_player_lifes);
 
     ventana->setView(*minimap);///SET VIEW MAP
     mapa->draw(ventana);
