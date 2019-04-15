aeab87d564366ef0780be75c927f6625045ea0f5
gambasconjamon <samjavaloyes96@gmail.com>
2019-04-15 12:46:23 +0200

    add/Animacion cuando coge turbo

--- src/Jugador.cpp
+++ src/Jugador.cpp
@@ -8,6 +8,27 @@ pState=new PhysicsState();
     pState->setActualState(150,250);
     /**Funcionamiento: le digo la ruta de la textura y el numero de animaciones**/
     jugador_sprite= new SuperSprite("assets/jugador/sp_alien_texture.png",4,0.6,true);
+    turbo= new SuperSprite("assets/jugador/turbo.png",4,0.6,true);
+
+    turbo->addFrame(sf::IntRect(27, 11,27, 58),0);
+    turbo->addFrame(sf::IntRect(84, 17,31, 48),0);
+    turbo->addFrame(sf::IntRect(147, 14,35, 54),0);
+    turbo->addFrame(sf::IntRect(209, 12,35, 56),0);
+    turbo->addFrame(sf::IntRect(27, 82,27, 58),1);
+    turbo->addFrame(sf::IntRect(83, 85,31, 48),1);
+    turbo->addFrame(sf::IntRect(138, 80,35, 54),1);
+    turbo->addFrame(sf::IntRect(199, 79,35, 56),1);
+    turbo->addFrame(sf::IntRect(11, 147,55, 27),2);
+    turbo->addFrame(sf::IntRect(74, 147,48, 31),2);
+    turbo->addFrame(sf::IntRect(128, 147,54, 35),2);
+    turbo->addFrame(sf::IntRect(188, 147,56, 35),2);
+    turbo->addFrame(sf::IntRect(11, 201,55, 27),3);
+    turbo->addFrame(sf::IntRect(74, 197,48, 31),3);
+    turbo->addFrame(sf::IntRect(128, 193,54, 35),3);
+    turbo->addFrame(sf::IntRect(188, 193,56, 35),3);
+
+    turbo->setSpeed(0.1);
+
     jugador_sprite->addFrame(sf::IntRect(50, 52,40, 44),0);//iz
     jugador_sprite->addFrame(sf::IntRect(2, 50,40, 46),0);
 
@@ -56,6 +77,7 @@ void Jugador::updateJugador(bool eRight,bool eLeft,bool eUp,bool eDown, sf::Time
     if(eRight)
     {
     jugador_sprite->setAnimation(1);
+    turbo->setAnimation(1);
         //La tecla Derecha está pulsada:
         x=potencia;
 
@@ -63,11 +85,13 @@ void Jugador::updateJugador(bool eRight,bool eLeft,bool eUp,bool eDown, sf::Time
     if(eLeft)
     {
     jugador_sprite->setAnimation(0);
+    turbo->setAnimation(0);
         x=-potencia;
         //La tecla Izquierda está pulsada:
     }
     if(eUp)
     {
+    turbo->setAnimation(2);
     jugador_sprite->setAnimation(2);
         y=-potencia;
         //La tecla Arriba está pulsada:
@@ -75,6 +99,7 @@ void Jugador::updateJugador(bool eRight,bool eLeft,bool eUp,bool eDown, sf::Time
     if(eDown)
     {
     jugador_sprite->setAnimation(3);
+    turbo->setAnimation(3);
         y=potencia;
         //La tecla Abajo está pulsada:
     }
@@ -101,6 +126,9 @@ void Jugador::drawJugador(sf::RenderWindow *w, double i)
 
     /** Posicion = (Estado_actual - Estado_pasado) * Interpolacion + Estado_pasado **/
     this->jugador_sprite->drawSuperSprite(this->getPhysicsState()->getPastState(),this->getPhysicsState()->getActualState(),w,i);
+    if(estado==1){
+    this->turbo->drawSuperSprite(this->getPhysicsState()->getPastState(),this->getPhysicsState()->getActualState(),w,i);
+    }
 
 }
 
